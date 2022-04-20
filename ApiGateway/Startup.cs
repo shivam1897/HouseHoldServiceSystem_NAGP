3f0dee36288f7eaa89d54a399d49ff7fa2c1520e
shivam1897 <shivam1897@gmail.com>
2022-04-20 11:40:58 +0530

    Added polly

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -23,8 +23,7 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot().AddConsul();
-            //services.AddOcelot().AddPolly();
+            services.AddOcelot().AddConsul().AddPolly();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
